4f1e94c2ab60a3415987ca86dc16e28fcfba5655
emclaren <elgin_mclaren@hotmail.com>
2015-10-14 14:29:01 -0700

    Loader Gif now working

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp'); // Load Gulp!
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
-
-
+var autoprefixer = require('gulp-autoprefixer');
 
  
 gulp.task('scss', function () {
   gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
     .pipe(gulp.dest('./css'));
 });
-
-
 
 
 gulp.task('browser-sync', function() {
@@ -21,10 +22,7 @@ gulp.task('browser-sync', function() {
        }
    });
 
+
 	gulp.watch('./scss/**/*.scss',['scss']);
     gulp.watch(["index.html", "js/*.js", "css/*.css"]).on('change', browserSync.reload);
 });
-
-
-//maybe chnage **/*.html to index/*.html
-
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function(){
+$(function(){
 
    // set some initial variables
    var photoData,
@@ -7,51 +7,50 @@ $(document).ready(function(){
        hashtagSearch,
        instagramUrl,
        $photoList = $('.photo-list');
+
+
+  $( document ).ajaxStart(function() {
+      $(".loading-gif").append('<img class="loader" src="images/ajax-loader.gif" alt="loading spinner">');
+});
+$( document ).ajaxStop(function() {
+   $(".loading-gif").remove(); });
+
    // when the form is submitted
    $('#search-form').on('submit', function(event) {
+       event.preventDefault();
 
-      event.preventDefault();
-
-
-  
-
-    // .complete(function(data) {
-    //          $('#spinner').remove();})
       // reset all the things
       $photoList.empty();
-      $('.please').remove();
-
-  
-
+      $('.searchLogo').remove();
       photoData, photoItems = '',
+      
       // get the search string
       hashtagSearch = $('#search-words').val().replace(" ", ''),
-      instagramUrl = "https://api.instagram.com/v1/tags/"+hashtagSearch+"/media/recent?client_id=f7d4d6f8007343d69a53ac5bfe36f964" 
+      instagramUrl = "https://api.instagram.com/v1/tags/"+hashtagSearch+"/media/recent?client_id=f7d4d6f8007343d69a53ac5bfe36f964"; 
+      
       // make the call to the endpoint
-
-        // $('#search-form').html('<img src="../images/ajax-loader.gif"> loading...');
-
-
       $.ajax({
          method: 'GET',
          url: instagramUrl,
          dataType: 'jsonp'
       })
+
+
       // if it works...
       .done(function(data) {
          photoData = data.data;
          if ( photoData.length !== 0 ) {
             $.each(photoData, function(key, value) {
-               photoItems += '<li class="wrappertest">';
+               photoItems += '<li class="photo-container">';
                   photoItems += '<ul>';
-                     photoItems += '<li class="picture">';
-                     photoItems += '<img src="' + value.images.standard_resolution.url+ '" />'
+                     photoItems += '<li class="main-image">';
+                     photoItems += '<img src="' + value.images.standard_resolution.url+ '" />';
                      photoItems += '</li>';  
-                     photoItems += '<li class="profile">';
-                     photoItems += '<div class="wraptest1">'; 
+                     photoItems += '<li class="image-info">';
+                     photoItems += '<div class="profile-photo">'; 
                      photoItems += '<img src="' + value.user.profile_picture+ '" />';
                      photoItems += '</div>'; 
-                     photoItems += '<div class="wraptest2">'; 
+                     photoItems += '<div class="username">'; 
                      photoItems += '<h1>' + value.user.username + '</h1>';
                      photoItems += '<div class="likes">';
                      photoItems += '<i class="fa fa-comments"></i>' + " " +value.comments.count + " "+'<i class="fa fa-heart"></i>' +" " + value.likes.count + '</p>';
@@ -71,6 +70,8 @@ $(document).ready(function(){
       .fail(function() {
          $photoList.append('<li>Sorry! There was a problem, please try again.</li>');
       });
+
+
    });
 });
 
